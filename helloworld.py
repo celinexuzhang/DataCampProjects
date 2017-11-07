b27446b06ccc415dda67c69940ee26ebe09adc0b
celinexuzhang <celinezhang1991@outlook.com>
2017-11-07 01:07:51 -0500

    changes

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,3 @@
-print("hello Python testchange")
+#first install matplotlib from Windows Commmend line "pip install matplotlib"
+import matplotlib.pylab as plt
+#tomorrowo morning continue import dataset from excel using xlrd
