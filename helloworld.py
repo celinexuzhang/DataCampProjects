4a8d73ca9c59032c68788c3f072486e0c1e34dad
celinexuzhang <celinezhang1991@outlook.com>
2017-11-07 08:19:58 -0500

    changes

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,8 @@
 #first install matplotlib from Windows Commmend line "pip install matplotlib"
 import matplotlib.pylab as plt
-#tomorrowo morning continue import dataset from excel using xlrd
+import xlrd
+file_location = "C:/Users/celzhang/Desktop/Analytics/Python/IntermediatetoPython/DataSets/brics.xlsx"
+workbook = xlrd.open_workbook(file_location)
+brics = workbook.sheet_by_index(0)
+#understanding the second column heading
+brics.cell_value(0,1)
